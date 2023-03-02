eade2f5555730cff9ffbda9c4860e9e6d538dfab
Michael <auger.michaell@gmail.com>
2023-03-02 14:54:17 +0100

    Projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Navbar from "./scenes/Navbar";
 import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
+import MySkills from "./scenes/MySkills";
+import Projects from "./scenes/Projects";
 import { useEffect, useState } from "react";
+import LineGradient from "./components/LineGradient";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
@@ -34,6 +37,15 @@ function App() {
           />
         )}
         <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      {/* A separator of sections */}
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Projects />
       </div>
     </div>
   );
