da2cd3fc05ca5d5d0143f8d14cc6ccc000a6ad07
ByronMike <auger.michaell@gmail.com>
2023-03-22 12:41:23 +0000

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Testimonials from "./scenes/Testimonials";
 import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 import { useEffect, useState } from "react";
 import LineGradient from "./components/LineGradient";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -55,6 +56,9 @@ function App() {
       </div>
       <div className="w-5/6 mx-auto">
         <Contact />
+      </div>
+      <div className="w-5/6 mx-auto">
+        <Footer />
       </div>
     </div>
   );
