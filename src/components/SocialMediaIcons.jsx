b83aed7d821d48edcbbd60feaf0490d276538fbd
ByronMike <auger.michaell@gmail.com>
2023-03-23 14:43:59 +0000

    Perso_Media_icons

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -3,7 +3,7 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.linkedin.com"
+        href="https://www.linkedin.com/in/auger-michael/"
         target="_blank"
         rel="noreferrer" // For older browsers (potential security issues)
       >
@@ -11,7 +11,15 @@ const SocialMediaIcons = () => {
       </a>
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.twitter.com"
+        href="https://github.com/ByronMike/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="github-link" src="../assets/github.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://twitter.com/ByronMike_"
         target="_blank"
         rel="noreferrer"
       >
@@ -19,19 +27,11 @@ const SocialMediaIcons = () => {
       </a>
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.facebook.com"
+        href="https://open.spotify.com/playlist/2bTY4IUI6BEcOU1GmLlLrP"
         target="_blank"
         rel="noreferrer"
       >
-        <img alt="facebook-link" src="../assets/facebook.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://www.instagram.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="instagram-link" src="../assets/instagram.png" />
+        <img alt="spotify-link" src="../assets/spotify.png" />
       </a>
     </div>
   );
