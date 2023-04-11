8f30d18eb28eac18b6603f8a96b7488c80cf593e
ByronMike <auger.michaell@gmail.com>
2023-04-11 10:53:40 +0000

    responsive pictures testimonials

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Testimonials = () => {
   const testimonialStyles = `mx-auto relative max-w-[400px] h-[350px] flex flex-col justify-end p-10 mt-48
-before:absolute before:top-[0px] before:left-1/2 lg:before:top-[-120px] lg:before:-ml-[110px] lg:before:left-1/2`;
+  before:absolute before:top-[-100px] xxs:before:top-[-50px] before:left-1/2 xs:before:absolute xs:before:top-[0px] xs:before:left-1/2 lg:before:top-[-120px] lg:before:-ml-[110px] lg:before:left-1/2`;
 
   return (
     <section id="témoignages" className="pt-32 pb-16">
@@ -24,7 +24,9 @@ before:absolute before:top-[0px] before:left-1/2 lg:before:top-[-120px] lg:befor
         </p>
         <LineGradient width="mx-auto w-2/4" />
         <p className="mt-10">
-        Découvrez ce que mes clients ou mes collaborateurs ont à dire sur mon travail. Voici quelques-uns de leurs témoignages sur les projets que nous avons réalisés ensemble.
+          Découvrez ce que mes clients ou mes collaborateurs ont à dire sur mon
+          travail. Voici quelques-uns de leurs témoignages sur les projets que
+          nous avons réalisés ensemble.
         </p>
       </motion.div>
 
@@ -42,9 +44,18 @@ before:absolute before:top-[0px] before:left-1/2 lg:before:top-[-120px] lg:befor
             visible: { opacity: 1, scale: 1 },
           }}
         >
-          <p className="font-playfair text-6xl">“<span className="font-playfair text-xl text-black">Charlène.H</span></p>
+          <p className="font-playfair text-6xl">
+            “
+            <span className="font-playfair text-xl text-black">Charlène.H</span>
+          </p>
           <p className="text-justify text-sm">
-            Michaël a réalisé un travail très satisfaisant pour mon site WordPress (<a href="www.charleneonthebeach.com">charleneonthebeach.com</a>). Il a été à l'écoute et réactif pour réaliser mon site web en tenant compte de mes besoins et de mes demandes, tout au long du projet. Je recommande vivement tes services à quiconque cherche un développeur web compétent et fiable. 
+            Michaël a réalisé un travail très satisfaisant pour mon site
+            WordPress (
+            <a href="www.charleneonthebeach.com">charleneonthebeach.com</a>). Il
+            a été à l'écoute et réactif pour réaliser mon site web en tenant
+            compte de mes besoins et de mes demandes, tout au long du projet. Je
+            recommande vivement tes services à quiconque cherche un développeur
+            web compétent et fiable.
           </p>
         </motion.div>
 
@@ -59,9 +70,16 @@ before:absolute before:top-[0px] before:left-1/2 lg:before:top-[-120px] lg:befor
             visible: { opacity: 1, scale: 1 },
           }}
         >
-          <p className="font-playfair text-6xl">“<span className="font-playfair text-xl text-black">Mathis.C</span></p>
+          <p className="font-playfair text-6xl">
+            “<span className="font-playfair text-xl text-black">Mathis.C</span>
+          </p>
           <p className="text-justify text-sm">
-          Je recommande fortement Michael pour ses compétences de développement mobile, son professionnalisme et sa capacité à travailler en équipe pour atteindre des objectifs communs. Son expertise sur React Native a été un véritable atout pour notre équipe, et son travail a été d’une grande qualité tout au long du projet.
+            Je recommande fortement Michael pour ses compétences de
+            développement mobile, son professionnalisme et sa capacité à
+            travailler en équipe pour atteindre des objectifs communs. Son
+            expertise sur React Native a été un véritable atout pour notre
+            équipe, et son travail a été d’une grande qualité tout au long du
+            projet.
           </p>
         </motion.div>
 
@@ -76,9 +94,17 @@ before:absolute before:top-[0px] before:left-1/2 lg:before:top-[-120px] lg:befor
             visible: { opacity: 1, scale: 1 },
           }}
         >
-         <p className="font-playfair text-6xl">“<span className="font-playfair text-xl text-black">Stéphane.C</span></p>
+          <p className="font-playfair text-6xl">
+            “
+            <span className="font-playfair text-xl text-black">Stéphane.C</span>
+          </p>
           <p className="text-justify text-sm">
-          Le travail réalisé par Michael Auger m'a permis de juger de ses qualités de réflexion, de rédaction et d'investigation auprès d'acteurs institutionnels ou financiers. Son goût marqué pour la recherche de nouvelles solutions, ses fortes convictions pour faire avancer les réflexions, son esprit toujours curieux ont été des facteurs clé de réussite.
+            Le travail réalisé par Michael Auger m'a permis de juger de ses
+            qualités de réflexion, de rédaction et d'investigation auprès
+            d'acteurs institutionnels ou financiers. Son goût marqué pour la
+            recherche de nouvelles solutions, ses fortes convictions pour faire
+            avancer les réflexions, son esprit toujours curieux ont été des
+            facteurs clé de réussite.
           </p>
         </motion.div>
       </div>
